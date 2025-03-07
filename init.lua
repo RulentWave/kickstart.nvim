39a3e63118195f4389df52ea2ad22d780c4347ed
Eric Hernandez <ejhndz91@gmail.com>
2025-03-06 23:00:38 -0500

    removed rust_analyzer as it is not needed with rustaceanvim plugin

--- init.lua
+++ init.lua
@@ -672,7 +672,7 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         pyright = {},
-        rust_analyzer = {},
+        --rust_analyzer = {}, -- not needed if using rustaceanvim
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
