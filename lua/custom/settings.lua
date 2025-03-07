2b3fa371f9f22771c3ec54b835e8c6648af00982
Eric Hernandez <ejhndz91@gmail.com>
2025-03-06 22:56:30 -0500

    changed indent line char, added lazydev, rustaceanvim

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -1,3 +1,7 @@
 vim.o.tabstop = 3
 vim.o.shiftwidth = 3
 vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>", { desc = "[U]ndo tree" })
+require("ibl").setup({ indent = { tab_char = "▍" } })
+require("lazydev").setup({
+	library = { "nvim-dap-ui" },
+})
